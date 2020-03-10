73b6f9a43e6b90f7cb2013df9d7016bbe30407f0
rodrilv <rott.9954@gmail.com>
2020-03-10 14:46:06 -0600

    Se agrego el componente de administradores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { APP_ROUTING } from "./app.routes";
+import { UserAdminComponent } from './components/user-admin/user-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAdminComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/usuario.ts
@@ -0,0 +1,7 @@
+export class UsuarioModel {
+    idRol: string;
+    idDireccion: string;
+    strNombre: string;
+    nmbCodigoEmpleado: number;
+    strContrasenia: string;
+}
