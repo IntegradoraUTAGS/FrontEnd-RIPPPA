6cecbcca72c9a15ce159fd81f2e2934cc18b958a
Rodrigo <rott.9954@gmail.com>
2020-03-30 13:56:36 -0600

    Arreglados detalles, falta modificar

--- src/app/components/administrador/administrador.component.ts
+++ src/app/components/administrador/administrador.component.ts
@@ -21,14 +21,13 @@ export class AdministradorComponent implements OnInit {
   mostrarAdmin(){
     this.adminService.mostrarAdmin().subscribe((result: any) => this.admin = result);
   }
-  registrarAdmin(admin){
-    this.adminService.registrarAdmin(admin).subscribe((result:any) => this.admin = result);
+  registrarAdmin(){
+    this.adminService.registrarAdmin(this.admin).subscribe((result:any) => this.admin = result);
   }
   eliminarAdmin(_id){
     this.adminService.eliminarAdmin(_id).subscribe((result:any) => this.admin = result);
   }
   actualAdmin(_id){
-    this.adminService.actualAdmin(_id).subscribe((result:any)=> this.admin = result);
+    this.adminService.actualAdmin(this.admin, _id).subscribe((result:any)=> this.admin = result);
   }
-
 }
