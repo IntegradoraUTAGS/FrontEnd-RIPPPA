3c77006b0909f16f2a114c43e799e2f3914f2ab7
Rodrigo <rott.9954@gmail.com>
2020-04-06 19:25:15 -0500

    Integracion de maestrias terminada pero aun no completada, avances por marco antonio y rodrigo sanchez

--- src/app/components/maestrias/maestrias.component.ts
+++ src/app/components/maestrias/maestrias.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MaestriaModel } from 'src/app/models/maestria';
+import { MaestriaService } from 'src/app/services/maestria.service';
+import Swal from 'sweetalert2';
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+ });
 
 @Component({
   selector: 'app-maestrias',
@@ -6,10 +16,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./maestrias.component.css']
 })
 export class MaestriasComponent implements OnInit {
+  maestria:MaestriaModel=new MaestriaModel();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private maestriaService:MaestriaService) { }
+  ngOnInit() {
+    this.obtenerMaestria();
   }
+
+  obtenerMaestria($event?) {
+    this.maestriaService.obtenerMaestria().then((maestrias: any) => {
+      this.maestria = maestrias.cont;
+    }).catch((err: any) => {
+      Toast.fire(err.error.msg, '', 'warning');
+    });
+  }
+
+  monstrarActualizar(idMaestria: string) {
+    this.maestria = idMaestria;
+  }
+
 
 }
