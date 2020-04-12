252c96ad228c63086278f5b998eb52f232395716
Natanael Acero <nataacero1@gmail.com>
2020-04-12 15:19:26 -0500

    Se corrigieron algunos errores

---
+++ src/app/models/convocatoria.ts
@@ -0,0 +1,12 @@
+export class ConvocatoriaModel {
+    idconvocatoria?: String;
+   idDireccion:string;
+   idProfesor:String;
+   idAcademia:string;
+   DateFecha:Date;
+   idPeriodo:string;
+   dateFechaDebeCubrirse:Date;
+   strTurno:string;
+   numHoras:number;
+
+}
