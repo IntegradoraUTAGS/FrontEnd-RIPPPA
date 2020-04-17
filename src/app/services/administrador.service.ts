76b74368b90ac8cf09f51d3e243b4616ad4a8824
Natanael Acero <nataacero1@gmail.com>
2020-04-17 16:41:45 -0500

    Se corrigio un error

--- src/app/services/administrador.service.ts
+++ src/app/services/administrador.service.ts
@@ -10,7 +10,7 @@ import { environment } from "../../environments/environment.prod";
 export class AdministradorService {
 
 
-    private url: string = 'http://localhost:3000/api/administrador'
+    private url: string = `${environment.urlProd}administrador`;
 
     constructor(private http: HttpClient) { }
 
@@ -18,17 +18,17 @@ export class AdministradorService {
         return this.http.post(this.url + 'login', administrador).toPromise();
     }
 
-    mostrarAdmin(){
+    mostrarAdmin() {
         return this.http.get(`${this.url}/obtener`).toPromise();
     }
-    registrarAdmin(administrador:AdministradorModel){
+    registrarAdmin(administrador: AdministradorModel) {
         return this.http.post(`${this.url}/registrar`, administrador).toPromise();
     }
-    eliminarAdmin(_id){
+    eliminarAdmin(_id) {
         return this.http.delete(`${this.url}/eliminar/:${_id}`);
     }
-    actualAdmin(administrador:AdministradorModel, _id:string){
-        return this.http.put(`${this.url}actualizar/:${_id}`,administrador);
+    actualAdmin(administrador: AdministradorModel, _id: string) {
+        return this.http.put(`${this.url}actualizar/:${_id}`, administrador);
 
     }
 }
